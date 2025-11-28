a82e76baedd5eed9c592fdeeb91e6ff15c2815a3
kishor <kishorebsc12@gmail.com>
2025-11-28 10:05:29 +0530

    added

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ if [ -z "$1" ]; then
     echo Input is excepted
     exit 1
 fi
-ansible-playbook -i $[1]--dev.kommanuthala.store, -e ansible_user=ec2-user -e ansible_password=DevOps321 roboshop.yml -e component_name=$1 -e env=dev
+ansible-playbook -i $[1]-dev.kommanuthala.store, -e ansible_user=ec2-user -e ansible_password=DevOps321 roboshop.yml -e component_name=$1 -e env=dev
